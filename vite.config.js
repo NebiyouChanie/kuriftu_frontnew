4fa6d88108e3f13f7a937b6538c3a8aa80438383
Nebiyou <cnebiyu@gmail.com>
2025-04-12 17:34:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: "0.0.0.0", // Required to expose to the outside world
+    port: Number(process.env.PORT) || 5173, // Render sets PORT env var
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: Number(process.env.PORT) || 4173,
+  },
 });
